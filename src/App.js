c64ab13c58c98747d03f63cb5d9afed303a8cb34
Dawid <dawid.polednik@gmail.com>
2019-03-21 20:05:41 +0100

    added a header with logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./App.scss";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import PrimarySearchAppBar from "./components/Header/Header";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <header>
+          <PrimarySearchAppBar />
+        </header>
+        <section>Posty</section>
+
+        <footer />
+      </div>
+    );
   }
 }
 
