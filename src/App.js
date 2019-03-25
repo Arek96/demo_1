65ebb324ee1ab231530fd61be5a2958a1a3327a0
Arek <arkadiuszosoba@gmail.com>
2019-03-25 16:45:38 +0100

    added react router and path to login page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import LoginPage from "./components/LoginPage/LoginPage";
+import { Router, Route } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authToken: undefined
+    };
+    this.passAuthToken = this.getAuthToken.bind(this);
+  }
+  getAuthToken(authToken) {
+    this.setState({
+      authToken: authToken
+    });
+  }
   render() {
     return (
-      <div>
-        <LoginPage />
-      </div>
+      <Router>
+        <Route
+          path="/login"
+          render={props => (
+            <LoginPage {...props} passAuthToken={this.getAuthToken} />
+          )}
+        />
+      </Router>
     );
   }
 }
