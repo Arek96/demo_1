1aa0d662f4a3ff71a0bda43078a75d3319701f82
Arek <arkadiuszosoba@gmail.com>
2019-03-31 23:26:15 +0200

    added margin to main

--- src/components/NotLogged/NotLogged.jsx
+++ src/components/NotLogged/NotLogged.jsx
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import styles from "./NotLogged.module.scss";
 import { Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
 
 const NotLogged = () => (
   <div className={styles.MainDiv}>
     <Typography variant="h5" className={styles.Message}>
       You are not logged in, to go any further and explore our awesome app{" "}
-      <Link to="/login">Logg in</Link>
+      <Button>
+        <Link to="/login">Logg in</Link>
+      </Button>
     </Typography>
   </div>
 );
