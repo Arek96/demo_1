bb61bc0d9997072dc869f00ece49effc5bf08f2b
Dawid <dawid.polednik@gmail.com>
2019-04-07 16:10:27 +0200

    update layout profil page

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import Card from "@material-ui/core/Card";
 import Avatar from '@material-ui/core/Avatar';
 import Button from "@material-ui/core/Button";
-import Paper from '@material-ui/core/Paper';
-// import CardActions from "@material-ui/core/CardActions";
+import classNames from "classnames";
 import CardContent from "@material-ui/core/CardContent";
 import { Grid, Typography, withStyles } from "@material-ui/core";
 import style from "../UserProfile/UserProfile.module.scss";
@@ -25,21 +24,16 @@ class UserProfile extends Component {
         const { classes } = this.props;
         return (
             <>
-                <Grid container direction='column'>
+                <Grid container direction='column' style={{ marginTop: '3rem' }}>
                     <Grid item
                     >
                         <Card
                             className={style.ProfileContainer}
-                            xs={10}
-                            sm={8}
-                            md={6}
-                            lg={4}
-                            xl={4}
                         >
                             <Avatar alt="Profile photo" src="https://cc-media-foxit.fichub.com/image/fox-it-mondofox/e8c0f288-781d-4d0b-98ad-fd169782b53b/scene-sottacqua-per-i-sequel-di-avatar-maxw-654.jpg" className={classes.avatar} />
                             <CardContent className={style.BioContainer}>
                                 <div className={style.ButtonContainer}>
-                                    <Typography variant='headline' align='justify' style={{ paddingTop: '10px' }} className={classes.typography}>dawidpolednik</Typography>
+                                    <Typography variant='headline' align='justify' style={{ paddingTop: '10px' }} className={classNames(classes.typography, classes.loginControl)}>dawidpolednik</Typography>
                                     <Button
                                         variant="contained"
                                         className={classes.edit}
--- src/components/UserProfile/UserProfile.styles.js
+++ src/components/UserProfile/UserProfile.styles.js
@@ -1,13 +1,5 @@
 const styles = theme => ({
-    card: {
-        minHeight: "70vh",
-        display: "flex",
-        justifyContent: "center",
-        boxShadow: "0 0 20px 0 rgba(0, 0, 0, 0.7)",
-        border: "3px solid rgba(0, 0, 0, 0.2)",
-        marginBottom: "9rem",
-        marginUp: "5rem"
-    },
+
     edit: {
         width: 120,
         height: 30,
@@ -15,16 +7,38 @@ const styles = theme => ({
         backgroundColor: "#3F51B5",
         marginLeft: '1.2rem',
         marginTop: '8px',
-        fontSize: "0.7rem"
+        fontSize: "0.7rem",
+        [theme.breakpoints.down("sm")]: {
+            width: '100%',
+            marginLeft: 0,
+        }
+    },
+    loginControl: {
+        [theme.breakpoints.down("sm")]: {
+            fontSize: '1.2rem'
+        },
+        [theme.breakpoints.down("sm")]: {
+            fontSize: '1.0rem'
+        },
     },
     avatar: {
         margin: '10px 70px 10px 20px',
         width: 170,
         height: 170,
+        alignSelf: 'center',
+        [theme.breakpoints.down("sm")]: {
+            width: 150,
+            height: 150,
+            margin: '5px 50px 10px 20px',
+        },
+        [theme.breakpoints.down("xs")]: {
+            width: 140,
+            height: 140,
+            margin: '5px 20px 10px 20px',
+        }
     },
     typography: {
         marginBottom: 10,
-        fontFamily: 'roboto'
     }
 
 });
