48db58fac4602c5df43fd939d0eb86cc4a38fc2f
Dawid <dawid.polednik@gmail.com>
2019-04-08 09:09:35 +0200

    update a photo list component

--- src/components/UserProfile/PostPhoto.jsx
+++ src/components/UserProfile/PostPhoto.jsx
@@ -6,26 +6,26 @@ let post = {
     id: 0,
     img: 'https://cc-media-foxit.fichub.com/image/fox-it-mondofox/e8c0f288-781d-4d0b-98ad-fd169782b53b/scene-sottacqua-per-i-sequel-di-avatar-maxw-654.jpg'
 }
-for (let i = 0; i < 5; i++) {
-
+for (let i = 0; i < 6; i++) {
     arrayPost.push(post);
     post.id++;
     console.log(post.id)
+
 }
 
 const PostPhoto = () => {
 
-    return arrayPost.map((element, index) => <Grid item key={index}
-        xs={10}
-        sm={8}
-        md={6}
-        lg={4}
-        xl={4}
-        className={style.postImage}>
-        <img src={element.img} alt="avatar" />
+    return <Grid container direction='row' justify="center" className={style.PhotosContainer} >
+        {arrayPost.map(element => <Grid item key={'gallery' + post.id}
+            xs={10}
+            sm={8}
+            md={6}
+            lg={4}
+            xl={4}
+            className={style.postImage}>
+            <img src={element.img} alt="avatar" />
+        </Grid>)}
     </Grid>
-    )
-
 }
 
 export default PostPhoto;
