eb184dde6adf71a9a8beb6685b82db008b152831
Dawid <dawid.polednik@gmail.com>
2019-04-08 20:32:28 +0200

    course modal

--- src/components/UserProfile/PostPhoto.jsx
+++ src/components/UserProfile/PostPhoto.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid } from "@material-ui/core";
 import style from "../UserProfile/UserProfile.module.scss";
+import PostModal from "../PostModal/PostModal";
 let arrayPost = [];
 let post = {
   id: 0
@@ -14,36 +15,43 @@ for (let i = 0; i < 30; i++) {
 }
 
 const PostPhoto = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const handlePhotoPost = () => {
+    setOpenModal(!openModal);
+  };
   return (
-    <Grid
-      container
-      direction="row"
-      justify="center"
-      className={style.PhotosContainer}
-    >
-      {arrayPost.map(element => (
-        <Grid
-          item
-          key={"gallery" + element.id}
-          xs={10}
-          sm={8}
-          md={6}
-          lg={4}
-          xl={4}
-          className={style.postImage}
-        >
-          <button
-            className="buttonGallery"
-            style={{
-              backgroundImage: `url("https://picsum.photos/200/300/?${
-                element.id
-              }")`
-            }}
-          />
-          {/* <img src={`${element.img}${element.id}`} alt="avatar" /> */}
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        className={style.PhotosContainer}
+      >
+        {arrayPost.map(element => (
+          <Grid
+            item
+            key={"gallery" + element.id}
+            xs={10}
+            sm={8}
+            md={6}
+            lg={4}
+            xl={4}
+            className={style.postImage}
+          >
+            <button
+              onClick={handlePhotoPost}
+              className="buttonGallery"
+              style={{
+                backgroundImage: `url("https://picsum.photos/200/300/?${
+                  element.id
+                }")`
+              }}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <PostModal open={openModal} />
+    </>
   );
 };
 
